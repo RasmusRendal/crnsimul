ae7a8d458a17516e51ad16f116b08b17270e2a08
Rasmus Rendal <rasmus@rend.al>
2020-02-21 13:34:19 +0100

    Added googletest framework

---
+++ tests/basictest.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+class BasicTest : public ::testing::Test {
+	protected:
+	  	virtual void SetUp() {
+	  	}
+
+	  	virtual void TearDown() {
+	    	// Code here will be called immediately after each test
+	    	// (right before the destructor).
+	  	}
+};
+
+TEST_F(BasicTest,twoValues){
+	const int x = 4;
+	const int y = 5;
+	EXPECT_EQ(9,x + y);
+}

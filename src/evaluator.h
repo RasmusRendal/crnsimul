24fc52b007c0cc90f3e6b737f9dd02f8a7f0a3ad
rkjar18 <48712402+rkjar18@users.noreply.github.com>
2020-02-24 08:47:56 +0000

    Add clang format step (#7)

--- src/evaluator.h
+++ src/evaluator.h
@@ -1,12 +1,13 @@
 #pragma once
 
 class Evaluator {
-	public:
-		Evaluator(ReactionNetwork network) : network(network) {}
-		State GetNextState();
-		int delta(int n);
-		std::vector<State> stateHistory;
-	protected:
-		virtual State GetNextStateInternal(ReactionNetwork network) = 0;
-		ReactionNetwork mNetwork;
+public:
+	Evaluator(ReactionNetwork network) : network(network) {}
+	State GetNextState();
+	int delta(int n);
+	std::vector<State> stateHistory;
+
+protected:
+	virtual State GetNextStateInternal(ReactionNetwork network) = 0;
+	ReactionNetwork mNetwork;
 };

1c8ab866f0c4a00c615e5a32588d6250832cc7d9
Rasmus Rendal <rasmus@rend.al>
2020-03-04 11:29:29 +0100

    Added another test

--- tests/crntest.cpp
+++ tests/crntest.cpp
@@ -15,11 +15,22 @@ protected:
 TEST_F(CrnTest, twoValues) {
 	driver drv;
 	drv.parse_string("a:=3;a->b;");
-	const int x = 4;
-	const int y = 5;
 	EXPECT_EQ(drv.Concentration["a"], 3);
 	EXPECT_EQ(drv.reactions.size(), 1);
 	auto reaction1 = drv.reactions[0];
 	EXPECT_EQ(reaction1.reactants["a"], 1);
 	EXPECT_EQ(reaction1.reactants["b"], 1);
+}
+
+TEST_F(CrnTest, parseTest) {
+	driver drv;
+	drv.parse_string("a:=3;a->b;b->0;");
+	EXPECT_EQ(drv.Concentration["a"], 3);
+	EXPECT_EQ(drv.reactions.size(), 2);
+	auto reaction1 = drv.reactions[0];
+	EXPECT_EQ(reaction1.reactants["a"], 1);
+	EXPECT_EQ(reaction1.products["b"], 1);
+	auto reaction2 = drv.reactions[1];
+	EXPECT_EQ(reaction1.reactants["a"], 1);
+	EXPECT_EQ(reaction1.products.size(), 0);
 }

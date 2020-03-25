2c837a53086357a9b5ada2e1109d930f3b0da561
Rasmus Rendal <rasmus@rend.al>
2020-03-25 23:09:31 +0100

    Ignore submodules too

--- gencov.sh
+++ gencov.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf /target/htmloutput || true
 mkdir /target/htmloutput
 cd /target/htmloutput
-gcovr -e /chem/bin/ -r /chem/ --html --html-details -o coverage.html
-gcovr -e /chem/bin/ -r /chem/ --html -o coverage_short.html
+gcovr -e /chem/bin/ -e /chem/submodules/ -r /chem/ --html --html-details -o coverage.html
+gcovr -e /chem/bin/ -e /chem/submodules/ -r /chem/ --html -o coverage_short.html

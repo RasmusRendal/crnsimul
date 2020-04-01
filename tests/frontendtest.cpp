057a45f51e8b156c1b033d6156fe11516214b231
imp4l3r <sogtrhs@gmail.com>
2020-04-01 14:05:14 +0200

    almost final commit, i think

--- tests/frontendtest.cpp
+++ tests/frontendtest.cpp
@@ -27,16 +27,3 @@ TEST_F(FrontendTest, PrintEvaluatedCSV) {
     std::string result = frontend.csvStream->str();
     EXPECT_EQ(expected, result);
 }
-
-/*TEST_F(FrontendTest, PrintCSV) {
-    driver drv;
-    std::stringstream *returnStream = nullptr;
-    drv.parse_string("a:=5; b:=3; c:=500; d:=0; a + c -> d;");
-    EvaluatorFrontend frontend;
-    std::string expectedHeader = "t,a,b,c,d\n";
-    std::string expectedRow0 = "0,5,3,500,0\n";
-    std::string expectedRow1 = "0.000100,4.750000,3.000000,499.750000,0.250000\n";
-    frontend.drv = &drv;
-    returnStream = new std::stringstream;
-    frontend.csvStream = returnStream;
-} */

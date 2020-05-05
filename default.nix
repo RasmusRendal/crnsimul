e47ae366ee2d7974c5a0c14b06770803f7d97716
Rasmus Rendal <rasmus@rend.al>
2020-05-05 14:15:28 +0200

    Add OpenRTPlotter dependencies to Nix
    This might not be the cleanest way to do it, but I couldn't/didn't want
    to spend time figuring out how to inherit from the submodules

--- default.nix
+++ default.nix
@@ -1,10 +1,15 @@
 with import <nixpkgs> {};
 
+let
+  unstable = import
+    (fetchTarball
+      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {};
+in
 stdenv.mkDerivation {
   name = "crnsimul";
   src = ./.;
 
-  buildInputs = [ cmake clang boost flex bison gnuplot gtest ];
+  buildInputs = [ cmake clang boost flex bison gnuplot gtest glew unstable.glfw freetype glm fontconfig ];
 
   buildPhase = "cmake . && make && ./bin/tests";
 
